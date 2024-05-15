d6b531a58aea1d7d21a1e45a8c186997560065e9
cccccccwx <1608344041@qq.com>
2024-05-15 09:48:46 +0800

    promises使用尝试

--- node/readfile.js
+++ node/readfile.js
@@ -2,14 +2,25 @@ const fs = require("fs")
 
 fs.readFile("./files/1.txt", "utf8", function (err, dataStr) {
     console.log(err)
-    //读取成功err为null
+    //读取成功时，err为null
     console.log("------")
     console.log(dataStr)
 })
 
 fs.readFile("./files/222.txt", "utf8", function (err, dataStr) {
     console.log(err)
-    //读取成功err为null
+
     console.log("------")
     console.log(dataStr)
+})
+
+fs.promises.readFile(
+    "./files/1.txt", {
+    encoding: 'utf-8'
+
+}).then(res => {
+    console.log("结果：", res)
+
+}).catch(err => {
+    console.log("错误：", err)
 })
